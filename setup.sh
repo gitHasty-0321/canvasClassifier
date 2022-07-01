ff3b545559f9e1934abd99d4ffb4802bcfb98d6b
Steven Hastings <steven.hastings0321@gmail.com>
2022-07-01 13:53:25 -0400

    final changes

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"steve.hastings0321@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
